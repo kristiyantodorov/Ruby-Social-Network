d99a54bdb84cee90da045115ad11a8e09205cb84
kristiyantodorov <todorovke@gmail.com>
2016-01-07 20:30:23 +0200

    Add compare method for Panda class

--- social_network.rb
+++ social_network.rb
@@ -15,6 +15,12 @@ class Panda
     @gender == 'female'
   end
 
+  def ==(other_panda)
+    @name == other_panda.name and
+    @email == other_panda.email and
+    @gender == other_panda.gender
+  end
+
   def to_s
     puts "#{@name}, #{@email}, #{@gender}"
   end
