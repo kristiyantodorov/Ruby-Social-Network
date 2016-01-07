bc3e8f6544741a6e0454cb35dd3478b2d853983a
kristiyantodorov <todorovke@gmail.com>
2016-01-07 20:21:12 +0200

    Add methods for Panda class

--- social_network.rb
+++ social_network.rb
@@ -7,6 +7,14 @@ class Panda
     @gender = gender
   end
 
+  def male?
+    @gender == 'male'
+  end
+
+  def female?
+    @gender == 'female'
+  end
+
   def to_s
     puts "#{@name}, #{@email}, #{@gender}"
   end
